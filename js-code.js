ed2f5de40415bb0fae7ae65f79ca1cef506031aa
KgothatsoN <kg.otha@hotmail.com>
2021-01-10 17:47:58 +0000

    Added draw board function and execute move method

--- js-code.js
+++ js-code.js
@@ -1,24 +1,44 @@
-class GameBoard {
+const Player_One = 'X';
+const Player_Two = 'O';
+
+
+class TicTacToe {
+  
+  handleSquareClick(event){
+    
+  }
+
+  executeMove(moveIndex){
+    if(this.board[moveIndex] == ""){
+      this.board[moveIndex] = this.currentPlayer;
+      this.currentPlayer = (this.currentPlayer == Player_One ? Player_Two  : Player_One);
+    }
+  }
 
   drawBoard(){
     document.body.innerHTML = "";
     let gameBoard = document.createElement('div');
     gameBoard.id = 'gameBoard';
     gameBoard.classList.add('board');
-
-    this.board.forEach((square, index)) => {
+    gameBoard.addEventListener('click', this.handleSquareClick.bind(this));
+   
+    this.board.forEach((square, index) =>{
       let squareElement = document.createElement('div');
       squareElement.id = index;
       squareElement.classList.add('square');
       gameBoard.appendChild(squareElement);
-    }
-  }
-  begin(){
-    this.board = ["", "", "",
-                  "", "", "",
-                  "", "", ""];
+
+    });
+    document.body.appendChild(gameBoard);
+  } 
+
+  start(){
+    this.board = ["","","",
+                  "","","",
+                  "","",""];
+    this.currentPlayer = Player_One;
     this.drawBoard();
   }
-
-
 }
+const game = new TicTacToe();
+game.start();
